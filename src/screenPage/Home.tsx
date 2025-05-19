6220d80587e56cc696b5e7985cbff39d3c0c634b
saga6569 <aristov6569@gmail.com>
2025-05-19 17:21:15 +0300

    add navigation

---
+++ src/screenPage/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <View>
+        <Text>Hello, world home</Text>
+        <Text>Welcome to React Native with Nat iveWind</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
---
+++ src/screenPage/Menu.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Menu = () => {
+  return (
+    <View>
+      <View>
+        <Text>Hello, world menu</Text>
+        <Text>Welcome to React Native with Nat iveWind</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Menu;
